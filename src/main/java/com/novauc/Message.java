35bdee5680f41373b4331199fb69b5cdcd955d99
Steve Jobs <peter@steves-mbp.nvstu.edu>
2017-03-15 17:04:29 -0400

    first two bullets

---
+++ src/main/java/com/novauc/Message.java
@@ -0,0 +1,37 @@
+package com.novauc;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "microblog")
+public class Message {
+    @Id
+    @GeneratedValue
+    int id;
+
+    @Column(nullable = false)
+    String author;
+
+    @Column(nullable = false)
+    int user_id;
+
+    @Column(nullable = false)
+    String text;
+
+    @Column(nullable = false)
+    String location;
+
+    @Column(nullable = false)
+    double time_seconds;
+
+    public Message(String author, int user_id, String text, String location, double time_seconds) {
+        this.author = author;
+        this.user_id = user_id;
+        this.text = text;
+        this.location = location;
+        this.time_seconds = time_seconds;
+
+    }
+
+    public Message() {}
+}
